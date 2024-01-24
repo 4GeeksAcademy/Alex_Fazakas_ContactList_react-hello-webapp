a25c49b19e0ede34a11bbf27c8f4cdc868c91f26
Alex Fazakas <144437816+alexcf04@users.noreply.github.com>
2024-01-24 19:42:09 +0000

    final

--- src/js/views/contact.js
+++ src/js/views/contact.js
@@ -1,1 +1,52 @@
-import React from "react";
+// Contact.js
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+import ContactCard from "../component/contactCard";
+
+const Contact = () => {
+  const { store, actions } = useContext(Context);
+
+  // Aquí se llama al método fetchContacts de actions para que se ejecute cuando se cargue el componente
+  useEffect(() => {
+    actions.fetchContacts();
+  }, [actions]);
+
+  // Llamo al actions para borrar el contacto pasando el id del contacto
+  const handleDeleteContact = (contactId) => {
+    actions.deleteContact(contactId);
+  };
+
+  // Quitar los puntos de la lista
+  const ulStyle = {
+    listStyleType: "none",
+  };
+
+  return (
+    <div className="p-4">
+      <div className="Titular1">
+        <h1 className="display-6 fw-bold ">Contact List</h1>
+        <Link to="/addContact" className="btn btn-primary">
+          Add contact
+        </Link>
+        <Link to="/" className="btn btn-success ms-3">
+          Home
+        </Link>
+      </div>
+      <div className="row row-cols-4 row-cols-md-2 g-4 Titular2" style={ulStyle}>
+        {store.contacts.map(
+          (contact) =>
+            contact.id && (
+              <ContactCard
+                key={contact.id}
+                contact={contact}
+                onDeleteContact={handleDeleteContact}
+              />
+            )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,13 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="text-center Titular  ">
+		<h1 className="display-1 fw-bold efectoOptico">Alex Fazakas</h1>
+		<Link className="display-5" to="/contact">
+			<h4>Check your Contacts</h4>
+		</Link>
 	</div>
 );
